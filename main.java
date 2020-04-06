31d27f4eb6e3e461396739e8ea8a893c29245a64
badat <you@example.com>
2020-04-06 15:12:29 +0700

    searchBinaryTree

---
+++ main.java
@@ -0,0 +1,19 @@
+package Tree.SeachBInaryTree;
+
+public class main {
+    public static void main(String[] args) {
+        //create a BST
+        BTS<String> tree = new BTS<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        //traverse tree
+        System.out.println("Inorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
